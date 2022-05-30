9f744bfc361ee8087b6c713a3c48b6885f552aec
Rajib Chandra Das <prorajib6@gmail.com>
2022-05-30 20:18:31 +0600

    post api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { application } = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,10 +27,17 @@ async function run() {
     const database = client.db('tourMaster');
     const destinationCollection = database.collection('destinations');
     // const bookingCollection = database.collection('placeOrder');
+
     app.get('/allbookings', async (req, res) => {
       const cursor = destinationCollection.find({});
-      const destionations = await cursor.toArray();
-      res.send(destionations);
+      const destinations = await cursor.toArray();
+    });
+
+    // insert new destination using POST api
+    app.post('/allbookings', async (req, res) => {
+      const destination = req.body;
+      const result = await destinationCollection.insertOne(destination);
+      res.json(result);
     });
   } finally {
     //  await client.close();
